0af474751764964f32f8bae6821c46601f147dac
nyuyz518 <lilyzhangyun@gmail.com>
2018-11-08 00:03:32 -0500

    Revert "Merge branch 'master' of https://github.com/nyuyz518/Redux-Youtube-ish"
    This reverts commit 706addd8a94fe82445f11bd16f7a4e6d9210aa30, reversing
    changes made to 706e62edde24a8a37dfa7c4b2eb46e91a8a6694b.

--- src/index.js
+++ src/index.js
@@ -1,45 +1,15 @@
-import _ from 'lodash';
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-import SearchBar from './components/search_bar';
-import VideoList from './components/video_list';
-import VideoDetail from './components/video_detail';
+import App from './components/app';
+import reducers from './reducers';
 
-const API_KEY = 'AIzaSyCss67mHmOKhccR4RcmyL0OxP-ArD0_jRs';
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      videos: [],
-      selectedVideo: null
-    }
-  }
-
-videoSearch(term){
-  YTSearch({key:API_KEY, term: term}, (videos) => {
-    this.setState({
-      videos: videos,
-      selectedVideo: videos[0]
-    });
-  });
-}
-
-
-  render(){
-    return (
-      <div>
-        <SearchBar onSearchBarChange={term => this.videoSearch(term)} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-          videos={this.state.videos}/>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('wa'));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
