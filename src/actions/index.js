7149e6002674e21d11ec26a87dbe86ebcb622b46
nyuyz518 <lilyzhangyun@gmail.com>
2018-11-13 23:27:40 -0500

    Revert "fixed show"
    This reverts commit f1e65052fbe1e8f10d4493106e098280321c21ab.

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,14 +3,12 @@ import axios from 'axios';
 export const FETCH_EVENTS = 'fetch_events';
 export const FETCH_EVENT = 'fetch_event';
 export const CREATE_EVENT = 'create_event';
-export const DELETE_EVENT = 'delete_event';
-
 
 const ROOT_URL = 'http://localhost:3000/api/v1';
 const API_KEY = '?key=papertiger1234';
 
 export function fetchEvents(){
-  const request = axios.get(`${ROOT_URL}/events${API_KEY}`);
+  const request = axios.get(`${ROOT_URL}/events`);
   return{
     type: FETCH_EVENTS,
     payload: request
@@ -19,7 +17,7 @@ export function fetchEvents(){
 
 
 export function createEvent(values, callback){
-  const request = axios.post(`${ROOT_URL}/events${API_KEY}`, values)
+  const request = axios.post(`${ROOT_URL}/events`, values)
     .then(()=>callback());
 
   return{
@@ -29,20 +27,9 @@ export function createEvent(values, callback){
 }
 
 export function fetchEvent(id){
-  const request = axios.get(`${ROOT_URL}/events/${id}${API_KEY}`);
+  const request = axios.get(`${ROOT_URL}/events/${id}`);
   return{
     type: FETCH_EVENT,
     payload: request
   };
-}
-
-export function deleteEvent(id, callback){
-  const request = axios.delete(`${ROOT_URL}/events/${id}${API_KEY}`)
-  .then(()=>callback());
-
-  return{
-    type: DELETE_EVENT,
-    payload: id
-  };
-
 }
--- src/components/events_show.js
+++ src/components/events_show.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux';
-import { fetchEvent, deleteEvent } from '../actions';
+import { fetchEvent } from '../actions';
 
 class EventsShow extends Component {
   componentDidMount(){
@@ -9,15 +9,8 @@ class EventsShow extends Component {
     this.props.fetchEvent(id);
   }
 
-  onDeleteClick(){
-    const {id} = this.props.match.params;
-    this.props.deleteEvent(id, () => {
-      this.props.history.push("/")
-    });
-  }
-
   render(){
-    const { event }  = this.props;
+    const { event } = this.props
 
     if(!event){
       return(
@@ -33,9 +26,7 @@ class EventsShow extends Component {
         <h5>Location: {event.location}</h5>
         <p>Description of the playdate: {event.description}</p>
         <button className="btn btn-primary pull-xs-left">Edit </button>
-        <button className="btn btn-danger pull-xs-center"
-          onClick={this.onDeleteClick.bind(this)}>
-        Delete </button>
+        <button className="btn btn-danger pull-xs-center">Delete </button>
       </div>
     );
   }
@@ -45,4 +36,4 @@ function mapStateToProps({ events },ownProps) {
   return {event: events[ownProps.match.params.id]}
 }
 
-export default connect(mapStateToProps,{fetchEvent, deleteEvent})(EventsShow);
+export default connect(mapStateToProps,{fetchEvent})(EventsShow);
