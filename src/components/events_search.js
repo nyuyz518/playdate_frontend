b868aa19fe8c17ea0288069a2c880a10e62277c2
nyuyz518 <lilyzhangyun@gmail.com>
2018-12-04 16:58:22 -0500

    in search component add two functions and handleFormSubmit, pass 2lat 2lng in searchEvents

--- src/components/events_search.js
+++ src/components/events_search.js
@@ -9,19 +9,20 @@ import "react-datepicker/dist/react-datepicker.css";
 
 class SearchForm extends Component {
 
-  renderDatePicker({input, placeholder, defaultValue, meta: {touched, error} }) {
+  renderDatePicker({input, label, placeholder, defaultValue, meta: {touched, error} }) {
     return (
         <div>
-            <DatePicker
-               selected={input.value.date}
-               onChange={(date)=>{
-                 return input.onChange({date: date});
-               }}
-               showTimeSelect
-               timeFormat="HH:mm"
-               timeIntervals={60}
-               dateFormat="MM/d, yyyy h:mm aa"
-               timeCaption="time"
+          <span> {label} </span>
+          <DatePicker
+             selected={input.value.date}
+             onChange={(date)=>{
+               return input.onChange({date: date});
+             }}
+             showTimeSelect
+             timeFormat="HH:mm"
+             timeIntervals={60}
+             dateFormat="MM/d, yyyy h:mm aa"
+             timeCaption="time"
             />
             <div className="text-help red">
               {touched ? error : " "}
@@ -30,8 +31,50 @@ class SearchForm extends Component {
     );
   }
 
+  renderTextField(field){
+    return(
+      <div className="form-group">
+        <label>{field.label}</label>
+          <input
+            className="form-control"
+            type="text"
+            {...field.input}
+          />
+          <div className="text-help red">
+            {field.meta.touched ? field.meta.error : " "}
+          </div>
+      </div>
+    );
+  }
+
+  mileToLat(latDis){
+    return (1/69) * latDis;
+  }
+
+  mileToLng(lngDis, lat){
+    const radLat = Math.PI * lat/180;
+    return lngDis/(Math.cos(radLat) * 69.172);
+  }
+
   handleFormSubmit(values){
-    this.props.searchEvents({time: values.search_time.date});
+    const radius = parseInt(values.search_radius);
+    const {coordinates} = values.search_location;
+    const latN = coordinates.lat + this.mileToLat(radius);
+    const latS = coordinates.lat - this.mileToLat(radius);
+    let latG = latS;
+    if(Math.abs(latN) < Math.abs(latS)){
+      latG = latN;
+    }
+    const lngE = coordinates.lng + this.mileToLng(radius, latG);
+    const lngW = coordinates.lng - this.mileToLng(radius, latG);
+
+    this.props.searchEvents({
+      time: values.search_time.date,
+      lat_n: latN,
+      lat_s: latS,
+      lng_e: lngE,
+      lng_w: lngW
+    });
   }
 
   render(){
@@ -39,13 +82,28 @@ class SearchForm extends Component {
     return (
       <div>
         <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-          <p>Search by Time</p>
+          <p>Search</p>
           <Field
-            label="search_time"
+            label="Time"
             name="search_time"
-            placeholder="Search by Time"
+            placeholder="Time"
             component={this.renderDatePicker}
           />
+          <Field
+            label="Location"
+            name="search_location"
+            placeholder="Near Location"
+            google={this.props.google}
+            component={ACInput}
+          />
+          <Field
+            label="Within"
+            name="search_radius"
+            placeholder="within"
+            component={this.renderTextField}
+          />
+          <span>Miles</span>
+          <p/>
           <button type="submit" className="btn btn-primary"> Search </button>
         </form>
       </div>
